d967d0a9375d2542ae5ebb61c9d611343edadc6a
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-04-27 17:46:27 +0200

    Add state for form inputs

--- src/components/create-task-form.tsx
+++ src/components/create-task-form.tsx
@@ -1,5 +1,6 @@
-"use client"
+'use client'
 
+import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
