6ae66bf5a512785435cb97a4ddcb873807ea920b
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-05-02 12:41:05 +0200

    feat: use skeleton component in list detail loading state

--- src/app/list/[id]/loading.tsx
+++ src/app/list/[id]/loading.tsx
@@ -1,9 +1,5 @@
-import { Loader2 } from 'lucide-react'
+import { TaskSkeleton } from '@/components/skeleton'
 
 export default function Loading() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
-    </div>
-  )
+  return <TaskSkeleton />
 }
