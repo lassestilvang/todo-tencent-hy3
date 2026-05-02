c19efe0e13ac26cedb75b8e1bd6f51f3114b15cd
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-05-02 13:03:17 +0200

    feat: add new task dialog triggered by keyboard shortcut

--- src/components/search-wrapper.tsx
+++ src/components/search-wrapper.tsx
@@ -1,16 +1,39 @@
-"use client"
+'use client'
 
-import { useState } from "react"
-import { SearchDialog } from "@/components/search-dialog"
-import { KeyboardShortcuts } from "@/components/keyboard-shortcuts"
+import { useState } from 'react'
+import { SearchDialog } from '@/components/search-dialog'
+import { CreateTaskForm } from '@/components/create-task-form'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog'
+import { KeyboardShortcuts } from '@/components/keyboard-shortcuts'
 
 export function SearchWrapper() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isSearchOpen, setIsSearchOpen] = useState(false)
+  const [isNewTaskOpen, setIsNewTaskOpen] = useState(false)
 
   return (
     <>
-      <KeyboardShortcuts onSearchOpen={() => setIsOpen(true)} />
-      <SearchDialog open={isOpen} onOpenChange={setIsOpen} />
+      <KeyboardShortcuts
+        onSearchOpen={() => setIsSearchOpen(true)}
+        onNewTask={() => setIsNewTaskOpen(true)}
+      />
+      <SearchDialog open={isSearchOpen} onOpenChange={setIsSearchOpen} />
+      <Dialog open={isNewTaskOpen} onOpenChange={setIsNewTaskOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Task</DialogTitle>
+            <DialogDescription>
+              Add a new task to your todo list
+            </DialogDescription>
+          </DialogHeader>
+          <CreateTaskForm />
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
